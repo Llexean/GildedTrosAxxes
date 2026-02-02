4f358727b4b171a5599f85f72729fe0f872e0789
Llexean <volkert.brusche.2@gmail.com>
2026-02-02 16:38:54 +0100

    Updated GilderTros.java

--- Java/src/main/java/com/gildedtros/GildedTros.java
+++ Java/src/main/java/com/gildedtros/GildedTros.java
@@ -3,7 +3,7 @@ package com.gildedtros;
 import java.util.Arrays;
 
 class GildedTros {
-    Item[] items;
+    CommonItem[] items;
 
     static final String[] LEGENDARY_NAME_LIST = {"B-DAWG Keychain"};
     static final String[] SMELLY_NAME_LIST = {"Duplicate Code", "Long Methods", "Ugly Variable Names"};
@@ -16,71 +16,76 @@ class GildedTros {
 
     static final int LEGENDARY_ITEMS_QUALITY = 80;
 
-    public GildedTros(Item[] items) {
-        Arrays.stream(items).forEach(this::clampQuality);
+    public GildedTros(CommonItem[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (Item item : items) {
-            if (Arrays.asList(LEGENDARY_NAME_LIST).contains(item.name)) {
-                updateLegendaryItem(item);
-                continue;
-            }
-
-            if (item.name.contains(BACKSTAGE_PASSES_ITEM_PREFIX)) {
-                updateBackstagePassesItem(item);
-            } else {
-                updateCommonItem(item);
-            }
-
-            item.sellIn--;
+        for (CommonItem item : items) {
+            item.updateQuality();
         }
     }
 
-    private void updateLegendaryItem(Item item) {
-        item.quality = LEGENDARY_ITEMS_QUALITY;
-        item.sellIn = 0;
-    }
-
-    private void updateBackstagePassesItem(Item item) {
-        if (item.sellIn <= 0) {
-            item.quality = ITEM_MIN_QUALITY;
-        } else if (item.sellIn <= 5) {
-            increaseQuality(item, 3);
-        } else if (item.sellIn <= 10) {
-            increaseQuality(item, 2);
-        } else {
-            increaseQuality(item, 1);
-        }
-    }
-
-    private void updateCommonItem(Item item) {
-        if (item.name.equals(GOOD_WINE_ITEM_NAME)) {
-            increaseQuality(item, 1);
-        } else if (Arrays.asList(SMELLY_NAME_LIST).contains(item.name)) {
-            decreaseQuality(item, 2);
-        } else {
-            decreaseQuality(item, 1);
-        }
-    }
-
-    private void increaseQuality(Item item, int amount) {
-        item.quality += amount;
-        clampQuality(item);
-    }
-
-    private void decreaseQuality(Item item, int amount) {
-        final int sellDateMultiplier = item.sellIn <= 0 ? 2 : 1;
-        item.quality -= amount * sellDateMultiplier;
-        clampQuality(item);
-    }
-
-    private void clampQuality(Item item) {
-        if (item.quality > ITEM_MAX_QUALITY) {
-            item.quality = ITEM_MAX_QUALITY;
-        } else if (item.quality < ITEM_MIN_QUALITY) {
-            item.quality = ITEM_MIN_QUALITY;
-        }
-    }
+//    public void updateQuality() {
+//        for (Item item : items) {
+//            if (Arrays.asList(LEGENDARY_NAME_LIST).contains(item.name)) {
+//                updateLegendaryItem(item);
+//                continue;
+//            }
+//
+//            if (item.name.contains(BACKSTAGE_PASSES_ITEM_PREFIX)) {
+//                updateBackstagePassesItem(item);
+//            } else {
+//                updateCommonItem(item);
+//            }
+//
+//            item.sellIn--;
+//        }
+//    }
+//
+//    private void updateLegendaryItem(Item item) {
+//        item.quality = LEGENDARY_ITEMS_QUALITY;
+//        item.sellIn = 0;
+//    }
+//
+//    private void updateBackstagePassesItem(Item item) {
+//        if (item.sellIn <= 0) {
+//            item.quality = ITEM_MIN_QUALITY;
+//        } else if (item.sellIn <= 5) {
+//            increaseQuality(item, 3);
+//        } else if (item.sellIn <= 10) {
+//            increaseQuality(item, 2);
+//        } else {
+//            increaseQuality(item, 1);
+//        }
+//    }
+//
+//    private void updateCommonItem(Item item) {
+//        if (item.name.equals(GOOD_WINE_ITEM_NAME)) {
+//            increaseQuality(item, 1);
+//        } else if (Arrays.asList(SMELLY_NAME_LIST).contains(item.name)) {
+//            decreaseQuality(item, 2);
+//        } else {
+//            decreaseQuality(item, 1);
+//        }
+//    }
+//
+//    private void increaseQuality(Item item, int amount) {
+//        item.quality += amount;
+//        clampQuality(item);
+//    }
+//
+//    private void decreaseQuality(Item item, int amount) {
+//        final int sellDateMultiplier = item.sellIn <= 0 ? 2 : 1;
+//        item.quality -= amount * sellDateMultiplier;
+//        clampQuality(item);
+//    }
+//
+//    private void clampQuality(Item item) {
+//        if (item.quality > ITEM_MAX_QUALITY) {
+//            item.quality = ITEM_MAX_QUALITY;
+//        } else if (item.quality < ITEM_MIN_QUALITY) {
+//            item.quality = ITEM_MIN_QUALITY;
+//        }
+//    }
 }
